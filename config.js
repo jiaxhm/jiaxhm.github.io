89f637218adcc11808ef2ce711f6a222e0b04a78
Neil <jiaxhm@163.com>
2026-04-08 15:53:18 +0800

    submit

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const USER_CONFIG = {
   role:       "副教授",
   university: "兰州交通大学",
   email:      "your.email@university.edu",
-  bio:        "主要从事机器学习和模式识别研究,先后主持国家自然科学基金1项,甘肃省青年科技基金1项,省部级开放课题1项,在国内外权威期刊及会议IEEE TIP、IEEE TFS、电子学报、IEEE FG等上发表学术论文20余篇,其中2篇论文入选ESI高被引,且单篇最高引用550余次,同时获得授权专利5项,并担任IEEE TFS、IEEE TCYB、IEEE TNNLS、IEEE TCSVT等国际期刊审稿人。",
+  bio:        "主要从事机器学习和模式识别研究，先后主持国家自然科学基金1项，甘肃省青年科技基金1项，省部级开放课题1项，在国内外权威期刊及会议IEEE TIP、IEEE TFS、IEEE SPL、IJFS、电子学报、IEEE ICASSP、IEEE FG、IJCNN等上发表学术论文20余篇，其中2篇论文入选ESI高被引，且单篇最高引用550余次，同时获得授权专利5项，并担任IEEE TFS、IEEE TCYB、IEEE TNNLS、IEEE TCSVT等国际期刊审稿人。",
   photo:      "assets/jiaxiaohong.jpg",   // optional: path to your photo, e.g. "assets/photo.jpg"
 
   stats: [
@@ -25,6 +25,8 @@ const USER_CONFIG = {
   },
 news: [
     { date: "2026.03.21", badge: "New",   text: "Two papers have been accepted to IJCNN 2026 (CCF-C). Congratulations to Zhiwei Xia and Fuhai Wang!" },
+    { date: "2026.01.16", badge: "New",   text: "One paper has been accepted to IEEE ICASSP 2026 (CCF-B). Congratulations to Yonghui Li!" },
+    { date: "2026.01.16", badge: "New",   text: "One paper has been accepted to IEEE ICASSP 2026 (CCF-B). Congratulations to Yonghui Li!" },
     { date: "2026.01.16", badge: "New",   text: "One paper has been accepted to IEEE ICASSP 2026 (CCF-B). Congratulations to Yonghui Li!" },
   ],
    publications: [
